19e598c896730b070656c1680d18df168bd5f807
RainbowBird <git@luoling.moe>
2025-09-17 01:49:37 +0800

    feat: jina link resolver

--- packages/core/src/message-resolvers/link-resolver.ts
+++ packages/core/src/message-resolvers/link-resolver.ts
@@ -1,17 +1,55 @@
 import type { MessageResolver, MessageResolverOpts } from '.'
-import type { CoreMessage } from '../utils/message'
 
 import { useLogger } from '@unbird/logg'
 import { Ok } from '@unbird/result'
+
+import { recordDocuments } from '../models/documents'
 
 export function createLinkResolver(): MessageResolver {
   const logger = useLogger('core:resolver:link')
 
   return {
-    run: async (_opts: MessageResolverOpts) => {
+    run: async (opts: MessageResolverOpts) => {
       logger.verbose('Executing link resolver')
 
-      return Ok([] as CoreMessage[])
+      for (const message of opts.messages) {
+        if (message.content) {
+          const links = message.content.match(/https?:\/\/\S+/g)
+          if (links && links.length > 0) {
+            for (const link of links) {
+              try {
+                const response = await fetch(`https://r.jina.ai/${link}`, {
+                  headers: {
+                    // Authorization: `Bearer ${JINA_API_KEY}`,
+                  },
+                })
+
+                if (!response.ok) {
+                  logger.error(`Failed to scrape link: ${link}`)
+                  continue
+                }
+
+                const doc = await response.text()
+
+                logger.debug('Scraped document')
+
+                await recordDocuments([{
+                  chatMessagesId: message.uuid,
+                  rawContent: doc,
+                  processedContent: doc,
+                  processedContentJiebaTokens: [],
+                  summary: doc,
+                }])
+              }
+              catch (err) {
+                logger.withError(err).error(`Error scraping link: ${link}`)
+              }
+            }
+          }
+        }
+      }
+
+      return Ok(opts.messages)
     },
   }
 }
